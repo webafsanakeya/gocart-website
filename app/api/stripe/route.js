b421db315ae8a0de7f0a80ec2062c4ba55468065
webafsanakeya <webafsakeya@gmail.com>
2025-10-13 01:10:46 +0600

    stripe webhooks route updated

--- app/api/stripe/route.js
+++ app/api/stripe/route.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 export async function POST(request){
     try {
         const body = await request.text()
-        const sig = request.get('stripe-signature')
+        const sig = request.headers.get('stripe-signature')
 
         const event = stripe.webhooks.constructEvent(body, sig, process.env.STRIPE_WEBHOOK_SECRET)
 
